dcf5f9434bbc6bcf3968e5ae374dddd7cbe9fdfa
Vinicius Gabriel <71111380+022Vinicius@users.noreply.github.com>
2025-12-11 15:42:43 -0300

    feat:criei a tabela de payments

--- database/init.sql
+++ database/init.sql
@@ -1,10 +1,21 @@
-create EXTENSION 'uuid-ossp'; -- uuid-ossp vai gerar os ids de pagamentos automaticamente
+create EXTENSION "uuid-ossp"; -- uuid-ossp vai gerar os ids de pagamentos automaticamente
+
 
 create table customers(
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name VARCHAR(255) NOT NULL,
     cnpj VARCHAR(14) NOT NULL UNIQUE,
     pix_key VARCHAR(255) NULL NULL,
-    pix_type VARCHAR(20) NOT NULL.
-    creat_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    pix_type VARCHAR(20) NOT NULL,
+    creat_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+create table payments(
+    txid VARCHAR(35) PRIMARY KEY,
+    customer_id NOT NULL, FOREIGN KEY, UUID REFERENCES customers(id),
+    status VARCHAR(20) NOT NULL, DEFAULT 'pending',
+    total_value NUMERIC(15,2) NOT NULL,
+    creat_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+
 );
