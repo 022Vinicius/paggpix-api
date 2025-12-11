3d624abe5aef6c326581a943ec8f80121fe9952a
Vinicius Gabriel <71111380+022Vinicius@users.noreply.github.com>
2025-12-11 16:04:23 -0300

    fix:antes a tabela payments estava com erros de sintaxe, mas agora isso foi corrigido

--- database/init.sql
+++ database/init.sql
@@ -1,19 +1,28 @@
-create EXTENSION "uuid-ossp"; -- uuid-ossp vai gerar os ids de pagamentos automaticamente
+create EXTENSION IF NOT EXISTS "uuid-ossp"; -- uuid-ossp vai gerar os ids de pagamentos automaticamente, 
+-- IF NOT EXISTS foi criado para evitar erro de verificação a cada execuçao do sql e visualização no pgadmin
 
 
-create table customers(
+
+-- apagar tabelas caso já existam para evitar erros 
+DROP TABLE IF EXISTS payments CASCADE;
+DROP TABLE IF EXISTS customers CASCADE;
+
+
+create TABLE IF NOT EXISTS customers(
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
     name VARCHAR(255) NOT NULL,
     cnpj VARCHAR(14) NOT NULL UNIQUE,
-    pix_key VARCHAR(255) NULL NULL,
+    pix_key VARCHAR(255) NOT NULL,
     pix_type VARCHAR(20) NOT NULL,
     creat_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-create table payments(
+
+
+create TABLE IF NOT EXISTS payments(
     txid VARCHAR(35) PRIMARY KEY,
-    customer_id NOT NULL, FOREIGN KEY, UUID REFERENCES customers(id),
-    status VARCHAR(20) NOT NULL, DEFAULT 'pending',
+    customer_id UUID NOT NULL REFERENCES customers(id) ON DELETE CASCADE --se o id da tabela customers for apagado, todos os pagamentos relacionados a esse cliente tb serao
+    status VARCHAR(20) NOT NULL DEFAULT 'pending',
     total_value NUMERIC(15,2) NOT NULL,
     creat_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 
